9f836cf18e1e60929540bb15b601cc82cfa9f6e9
DuhaOdat <odatduha@gmail.com>
2024-07-02 22:27:04 +0300

    opp task solution 2-7-2024

---
+++ oop_task/2-7-2024-task1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_7_2024
+{
+
+    
+
+    internal class Program
+    {
+
+        class Car
+        {
+            //proparities
+            public int Make { get; set; }
+            public int Year { get; set; }
+            public string Type { get; set; }
+            public double Price { get; set; }
+            public int Model { get; set; }
+            public int PalletNo { get; set; }
+            public string Color { get; set; }
+
+            //constractor
+            public Car( int Make,int Year,string Type,double Price,int Model,int PalletNo,string color )
+            { 
+                this.Make = Make;
+                this.Year = Year;
+                this.Type = Type;
+                this.Price = Price;
+                this.Model = Model;
+                this.PalletNo = PalletNo;
+                this.Color = color;
+            }
+
+            //methods
+            public void DisplayInfo(int make, int year)
+            {
+                Console.WriteLine($"Car info are :{make},{year}");
+            }
+
+            public void DisplayInfo(int make)
+            {
+                Console.WriteLine($"car make is :{make}");
+            }
+
+
+            public virtual void Display()
+                
+            {
+                Console.WriteLine("Welcome Car");
+            }
+
+
+        }
+
+         class BMW : Car
+        {
+
+            public BMW(int Make, int Year, string Type, double Price, int Model, int PalletNo, string color)
+                :base( Make,  Year,  Type, Price, Model, PalletNo, color)
+            {
+                
+            }
+            public override void Display()
+            {
+                Console.WriteLine("Welcome BMW");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            BMW bmw = new BMW(1999, 2002, "BMW", 1500.45, 2024, 123456, "red");
+            bmw.Display();
+            
+        }
+    }
+}
---
+++ oop_task/2-7-2024-task2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_7_2024
+{
+    internal class Program
+    {
+        interface ISound
+        {
+            void MakeSound();
+        }
+
+        abstract class Animal
+        {
+            public string Name { get; set; }
+
+            public Animal(string Name)
+            {
+                this.Name = Name;
+            }
+            public abstract void Eat();
+
+            public void Sleep()
+            {
+                Console.WriteLine($"animal name is {Name} is sleeping");
+            }
+        }
+
+        class Dog : Animal, ISound
+        {
+            public Dog(string Name):base(Name) { }
+            public override void Eat()
+            {
+                Console.WriteLine($"animal name is {Name} is eating");
+            }
+
+           public void MakeSound()
+            {
+                Console.WriteLine($" animal name is {Name} and the soud is : Woof1");
+            }
+
+        }
+        class Cat : Animal, ISound
+        {
+            public Cat(string Name) : base(Name) { }
+            public override void Eat()
+            {
+                Console.WriteLine($"animal name is {Name} is eating");
+            }
+
+          public  void MakeSound()
+            {
+                Console.WriteLine($" animal name is {Name} and the soud is : Mewo");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("ddddd");
+            dog.Eat();
+            dog.Sleep();
+            dog.MakeSound();
+            Cat cat = new Cat("ccccc");
+            cat.Eat();
+            cat.Sleep();
+            cat.MakeSound();
+        }
+    }
+}
